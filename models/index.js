6764f13147afd90722998c07862ac2ec1091dc10
StgoWF <155189000+StgoWF@users.noreply.github.com>
2024-06-24 01:33:56 -0400

    Set up associations between models

--- models/index.js
+++ models/index.js
@@ -1,20 +1,35 @@
-// import models
+// Import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belong to Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belong to many Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belong to many Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
   Category,
   Tag,
-  ProductTag,
+  ProductTag
 };
